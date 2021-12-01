dbf93de497daaf17aced8a1e4030b2e85adf997e
Castro456 <castrosid456@gmail.com>
2021-12-01 21:00:58 +0530

    added xmlhttprequest to send mutliple post request

--- process.php
+++ process.php
@@ -2,21 +2,22 @@
 
 // echo "Processing";
 
-$conn =mysqli_connect('localhost','root','giveaccess','ajax_test');
+// $conn =mysqli_connect('localhost','root','giveaccess','ajax_test');
 
 if(isset($_POST['postname']))
 {
-    $name = mysqli_real_escape_string($conn,$_POST['postname']);
+    // $name = mysqli_real_escape_string($conn,$_POST['postname']);
     echo "Your name from POST is:" .$_POST['postname'];
-    $query = "INSERT INTO test(names) VALUES ('$name')";
-    if(mysqli_query($conn,$query))
-    {
-        echo "User Added";
-    }
-    else
-    {
-        echo "Problem occured";
-    }
+    echo "Your name from POST is 1:" .$_POST['postname1'];
+    // $query = "INSERT INTO test(names) VALUES ('$name')";
+    // if(mysqli_query($conn,$query))
+    // {
+    //     echo "User Added";
+    // }
+    // else
+    // {
+    //     echo "Problem occured";
+    // }
 }
 
 if(isset($_GET['pname']))
